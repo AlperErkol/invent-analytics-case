51814ee4031a185764cb955929b024330adebcbe
Alper Erkol <erkol.alper@yahoo.com>
2024-10-06 05:43:28 +0300

    chore: remove unused folders & packages

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import HomePage from "./pages/home-page";
-
-test("renders learn react link", () => {
-  render(<HomePage />);
-  const linkElement = screen.getByText("home-page");
-  expect(linkElement).toBeInTheDocument();
-});
